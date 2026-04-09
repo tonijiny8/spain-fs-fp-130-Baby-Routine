1e07a6672dd75564244fa3e73e2fcc9dbf259b9a
tonijiny8 <tonijinyang@gmail.com>
2026-04-09 22:17:29 +0000

    he hecho el log donde se registran las tareas pero solo el nombre

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -11,6 +11,7 @@ import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import { Registro } from "./pages/Registro";
 import { Login } from "./pages/Login";
+import { FamiliaRutina } from "./pages/FamiliaRutina";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -30,6 +31,7 @@ export const router = createBrowserRouter(
 
         <Route path="/pages/Registro" element={<Registro />} />
         <Route path="/pages/Login" element={<Login />} />
+        <Route path="/familia" element={<FamiliaRutina />} />
 
       </Route>
     )
